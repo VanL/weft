d4c04b3731471bb6b7cd0d639604699903973d2a
VanL <van@ospo.co>
2026-04-09 06:32:51 -0500

    Release 0.7.3

--- weft/_constants.py
+++ weft/_constants.py
@@ -32,7 +32,7 @@ from simplebroker import resolve_config as resolve_broker_config
 # VERSION INFORMATION
 # ==============================================================================
 
-__version__: Final[str] = "0.7.2"
+__version__: Final[str] = "0.7.3"
 """Current version of Weft."""
 
 # ==============================================================================
